79e9643c0eb71b3b8123244254cbd40383b05d59
WhidRubeld <whidrubeld@gmail.com>
2021-08-08 15:24:04 +0300

    root point upgrade

--- App.tsx
+++ App.tsx
@@ -1,24 +1,25 @@
-import 'react-native-gesture-handler'
+import App from '@app'
 import { StatusBar } from 'expo-status-bar'
+import moment from 'moment'
 import React from 'react'
+import { Platform, UIManager } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import 'moment/locale/ru'
 
-import useCachedResources from './hooks/useCachedResources'
-import useColorScheme from './hooks/useColorScheme'
-import Navigation from './navigation'
+moment.locale('ru')
 
-export default function App() {
-  const isLoadingComplete = useCachedResources()
-  const colorScheme = useColorScheme()
+if (
+  Platform.OS === 'android' &&
+  UIManager.setLayoutAnimationEnabledExperimental
+) {
+  UIManager.setLayoutAnimationEnabledExperimental(true)
+}
 
-  if (!isLoadingComplete) {
-    return null
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    )
-  }
+export default function Boot() {
+  return (
+    <SafeAreaProvider>
+      <App />
+      <StatusBar style="dark" />
+    </SafeAreaProvider>
+  )
 }
