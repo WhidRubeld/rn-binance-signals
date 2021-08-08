ab9f16b425c87ed1e028e76e94d721ec5dc4f0f9
WhidRubeld <whidrubeld@gmail.com>
2021-08-08 14:26:23 +0300

    setting up env

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,46 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
-  };
-};
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@assets': './assets',
+            '@app': './src/app',
+            '@components': './src/components',
+            '@constants': './src/constants',
+            '@helpers': './src/helpers',
+            '@hooks': './src/hooks',
+            '@interfaces': './src/interfaces',
+            '@navigation': './src/app/navigation',
+            '@providers': './src/providers',
+            '@screens': './src/screens',
+            '@services': './src/services',
+            '@store': './src/store',
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      ],
+    ],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
+  }
+}
