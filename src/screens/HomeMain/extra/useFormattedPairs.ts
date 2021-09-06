b4139e8101748559b87507064a539922e8a41a82
WhidRubeld <whidrubeld@gmail.com>
2021-09-06 13:01:16 +0300

    abs sort

--- src/screens/HomeMain/extra/useFormattedPairs.ts
+++ src/screens/HomeMain/extra/useFormattedPairs.ts
@@ -13,26 +13,31 @@ export default function useFormattedPairs() {
     let lastLaunch = null
     const lastCheck = lastResultsCheck ? unix2time(lastResultsCheck) : null
 
-    const res = pairs.map((pair) => {
-      let value = null
-      const data = results.find((v) => {
-        return v.pair.first === pair.first && v.pair.second === pair.second
+    const res = pairs
+      .map((pair) => {
+        let value = null
+        const data = results.find((v) => {
+          return v.pair.first === pair.first && v.pair.second === pair.second
+        })
+
+        if (data && data.results.length) {
+          const { sma7, sma25, timestamp } =
+            data.results[data.results.length - 1]
+
+          lastLaunch = unix2time(timestamp)
+
+          value = parseFloat(((sma7 / sma25 - 1) * 100).toFixed(4))
+        }
+
+        return {
+          data: pair,
+          text: `${pair.first} / ${pair.second}`,
+          value,
+        }
       })
-
-      if (data && data.results.length) {
-        const { sma7, sma25, timestamp } = data.results[data.results.length - 1]
-
-        lastLaunch = unix2time(timestamp)
-
-        value = parseFloat(((sma7 / sma25 - 1) * 100).toFixed(4))
-      }
-
-      return {
-        data: pair,
-        text: `${pair.first} / ${pair.second}`,
-        value,
-      }
-    })
+      .sort((a, b) => {
+        return Math.abs(a.value || 0) - Math.abs(b.value || 0)
+      })
 
     return { lastLaunch, pairs: res, lastCheck }
   }, [pairs, results, lastResultsCheck])
